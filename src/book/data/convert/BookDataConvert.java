60332b76eead350283af5a917f5413337af95d18
sabbir <s.ahmed@horoppa.com>
2015-04-19 00:07:52 +0600

    add necessary comment

--- src/book/data/convert/BookDataConvert.java
+++ src/book/data/convert/BookDataConvert.java
@@ -11,12 +11,17 @@ public class BookDataConvert {
 	private static BookDataConvertService service = new BookDataConvertServiceImpl();
 	
 	public static void main(String[] args) {
-		//String fileName = args[0];
-		String fileName = "resource/jsonInput1.txt";
+		String fileName = args[0];
+		//String fileName = "resource/jsonInput1.txt";
+		
 		System.out.println("Reading input ...");
 		System.out.println("++++");
+		
+		//Check when reading input file, any error occurred or not.
 		String readFile = service.readDataFromFile(fileName);
 		if(readFile != null){
+			
+			//Detect data format from the input file.
 			String detectFormat = service.detectDataFormat(readFile);
 			if(detectFormat.equals("TXT")){
 				System.out.println(service.readFileOutputFormat(readFile));
@@ -31,11 +36,15 @@ public class BookDataConvert {
 			}
 			else{
 				System.out.println("Book data is in " + detectFormat + " format");
+				
+				//Get configuration from properties file.
 				Properties prop = BookDataProperties.getConvertFormatDetails();
 				String targetFormat = prop.getProperty("targetFormat");
 				System.out.println("Converting to " + targetFormat.toUpperCase() + " format");
 				System.out.println("Here is the output...");
 				System.out.println("++++");
+				
+				//Checking target format & detect format.
 				if(targetFormat.toUpperCase().equals(detectFormat)){
 					System.out.println(readFile);
 				}
